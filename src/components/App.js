26981fe715a1c88e12528132ef1e264a85f2f74f
Michael Elfassy <mike.elfassy@gmail.com>
2023-03-05 09:37:12 -0500

    commit tree c197cee62a799152bfce55583a95ee8c705837a6

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+
+    const API_URL = "https://dog.ceo/api/breeds/image/random"
+    const [image, setImage] = useState(null)
+
+    useEffect(() => {
+        fetch(API_URL)
+        .then(response => response.json())
+        .then(data => setImage(data.message))
+    }, [])
+
+    if (!image) {return (<p>Loading...</p>)}
+
+    return (
+        <img src={image} alt="A Random Dog"></img>
+    )
+}
+
+export default App;
